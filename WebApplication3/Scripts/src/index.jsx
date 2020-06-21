c888bd2f4781de01532f5abe528b929043ec1ef1
redy159 <36959093+redy159@users.noreply.github.com>
2020-06-22 05:09:00 +0700

    First Commit

---
+++ WebApplication3/Scripts/src/index.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './test.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Test/>
+                <div className="commentBox">Hello, world! I</div>
+            </React.Fragment>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
