64463fe410e6bba28938e2e3a413acca0bdcd810
touken2411 <43944044+touken2411@users.noreply.github.com>
2020-07-06 00:24:05 +0700

    get menu bug

--- WebApplication3/Scripts/src/Component/Header.jsx
+++ WebApplication3/Scripts/src/Component/Header.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import './../../../Content/header.css'
 export default class Header extends React.Component {
+    componentDidMount() {
+        fetch('/api/Manager/GetSportMenu', {
+            method: "Get",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state.data)
+        })
+            .then(response => {response.text() ? response.json(): {}});
+    }
 
     render() {
+        console.log(this.state.data? this.state.data : "000");
         return (
             <div>
                 <div class="navigation">
                     <div class="container">
                         <div class="row">
                             <ul class="col flex justify-content-between">
-                                <li class="has-dropdown">
-                                    <a ><strong>Football</strong></a>
-                                    <ul class="level-2">
+                            
+                            <p>Hello, {person.name} from {person.country}!</p>
+                                <li class="dropdown">
+                                    <a><strong>Football</strong></a>
+                                    <ul class="dropdown-content">
                                         <li><a href="">Football Helmets</a></li>
                                         <li><a href="">Facemasks</a></li>
                                         <li><a href="">Gloves</a></li>
