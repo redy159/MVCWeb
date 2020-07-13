3efb367771f4d8dad3efb123befe41e06f3df4f4
redy159 <36959093+redy159@users.noreply.github.com>
2020-07-13 21:36:35 +0700

    add jsx to new pages

--- WebApplication3/Scripts/src/ProductByCategory.jsx
+++ WebApplication3/Scripts/src/ProductByCategory.jsx
@@ -1,1 +1,16 @@
-﻿
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ProductByCategory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            )
+    }
+}
+
+ReactDOM.render(<ProductByCategory />, document.getElementById("root"));
--- WebApplication3/Scripts/src/ProductDetail.jsx
+++ WebApplication3/Scripts/src/ProductDetail.jsx
@@ -1,1 +1,16 @@
-﻿
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ProductDetail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            )
+    }
+}
+
+ReactDOM.render(<ProductDetail />, document.getElementById("root"));
