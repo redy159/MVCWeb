6d3eedaca8e350114dd500f1e03222058d7c32f1
NguyenDuyPhuoc150799 <phuocnt150799@gmail.com>
2020-07-14 14:05:00 +0700

    modal

--- WebApplication3/Scripts/src/Component/Header.jsx
+++ WebApplication3/Scripts/src/Component/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { Modal } from "react-responsive-modal";
 import './../../../Content/header.css'
-class Header extends React.Component {
+class Header extends React.Component {  
     constructor(props)
     {
         super(props)
-        this.state = {}
+        this.state = {
+            sign: false,
+            login: false,
+        }     
     }
+
+
     componentDidMount() {
         fetch('/api/Manager/GetSportMenu', {
             method: "Get",
@@ -20,36 +26,95 @@ class Header extends React.Component {
         
     }
 
+    onOpenModal  () {
+        this.setState({ sign: true });
+    };
+
+    onOpenModalLogin () {
+        this.setState({ login: true });
+    };
+
+    onCloseModal () {
+        this.setState({ sign: false });
+    };
+
+    onCloseModalclose (){
+        this.setState({ login: false });
+    };
+   
+
     render() {
+        const { login, sign } = this.state;
+       
         if (!this.state.data) return null;
         return (
             <React.Fragment>
+ 
                 <div class="container flex flex-row pl-3 pr-3">
                     <div class="logo">
-                        <a><h2>Sport <br/>Equipment</h2></a>
+                        <a><h2>Sport <br />Equipment</h2></a>
                     </div>
-                    <button type="button" class="btn btn-light log-in">Đăng nhập</button>
-                    <button type="button" class="btn btn-light sign-up">Đăng ký</button>
+                    <button type="button" class="btn btn-light log-in" onClick={() => { this.onOpenModalLogin }} >Đăng nhập</button>
+                    <button type="button" class="btn btn-light sign-up" onClick={() => { this.onOpenModal }}>Đăng ký</button>           
                 </div>
                 <div class="navigation">
                     <div class="container">
                         <div class="row">
                             <ul class="col flex justify-content-between">
-                            {this.state.data.map((item) => (
-                                <li class="dropdown">
-                                    <a><strong>{item.Name}</strong></a>
-                                    <ul class="dropdown-content">
-                                        {item.Categories.map((cate) => (
-                                            <li><a href="">{cate.Name}</a></li>
-                                        ))}
-                                    </ul>
-                                </li>
-                            ))}
+                                {this.state.data.map((item) => (
+                                    <li class="dropdown">
+                                        <a><strong>{item.Name}</strong></a>
+                                        <ul class="dropdown-content">
+                                            {item.Categories.map((cate) => (
+                                                <li><a href="">{cate.Name}</a></li>
+                                            ))}
+                                        </ul>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                     </div>
                 </div>
-            </React.Fragment>
+
+                {/* Sign up model */}
+
+                <Modal open={sign} onClose={this.onCloseModal}>
+                    <div className="modal-body">
+                        <h2>Sign up<span> Free!</span></h2>
+                        <div className="form-group">
+                            <input className="form-control" type="text" name="name" id="name" placeholder="First Name" required="" autocomplete="off" aria-required="true" />
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type="email" name="email" placeholder="E-mail" required="" autocomplete="off" aria-required="true" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" name="pass" className="form-control" placeholder="Password" required="" autocomplete="off" aria-required="true" />
+                        </div>
+                        <input className="btn btn-light sign-up" type="button" value="Sign Up" />
+
+                    </div>
+                </Modal>
+
+                {/* <!-- signUp End -->
+                  <!-- login --> */}
+
+                <Modal open={login} onClose={this.onCloseModalclose}>
+
+                    <div className="modal-body">
+                        <h2>Login and Get <span>Started</span></h2>
+                        <span className="subtitle">Just fill in the form below</span>
+                        <form className="container" novalidate="novalidate">
+                            <div className="form-group">
+                                <input className="form-control" type="email" name="email" placeholder="E-mail" required="" autocomplete="off" aria-required="true" />
+                            </div>
+                            <div className="form-group">
+                                <input type="password" name="pass" className="form-control" placeholder="Password" required="" autocomplete="off" aria-required="true" />
+                            </div>
+                            <input className="btn btn-light log-in" type="button" value="Login" />
+                        </form>
+                    </div>
+                </Modal>
+        </React.Fragment>
         )
     }
 }
--- WebApplication3/ShopWeb.Designer.cs
+++ WebApplication3/ShopWeb.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Master\source\repos\WebApplication3\WebApplication3\ShopWeb.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\HP\Desktop\CN.NET\MVCWeb\WebApplication3\ShopWeb.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
