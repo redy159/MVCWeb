9124cab9033eaf1eeb0e114d1dc73071ea269b7b
redy159 <36959093+redy159@users.noreply.github.com>
2020-07-15 15:12:53 +0700

    add User.password

--- WebApplication3/User.cs
+++ WebApplication3/User.cs
@@ -26,6 +26,7 @@ namespace WebApplication3
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public int UserType { get; set; }
+        public string Password { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ProductReview> ProductReviews { get; set; }
